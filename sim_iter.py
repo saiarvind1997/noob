c1593b0f46d5f824ea6f19dad2634cedf034b795
sai aravind <saiarvind1997@gmail.com>
2019-12-11 23:26:55 +0530

    sim_iter

--- sim_iter.py
+++ sim_iter.py
@@ -1,6 +1,17 @@
+c=ord('a')
+d=ord('z')+1
+while c<d:
+    i=1
+    while i<10:
+        print'{}{}'.format(chr(c),i)
+        i+=1
+    c+=1
 
+#alternate code with for loops:
+"""
 x= ord("a")
 y= ord("z")+1
 for c in range(x,y):
     for i in range(1,10):
         print "{}{}".format(chr(c),i)
+"""
