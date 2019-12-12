b2f1d8d517e304c26912f15c11aadf1ecc870f38
sai aravind <saiarvind1997@gmail.com>
2019-12-12 16:46:03 +0530

    random color

--- sim_ran.py
+++ sim_ran.py
@@ -1,12 +1,13 @@
 import random
-a=ord('a')
-z=ord('z')+1
-color=['Violet','Indigo','Blue','Green','Yellow','Orange','Red']
-
-while a<z:
-    i=1
-    while i<10:
-        ran_color=random.choice(color)
-        print'{}{},{}'.format(chr(a),i,ran_color)
-        i+=1
-    a+=1
+with open("color.txt","w+") as f:
+    a=ord('a')
+    z=ord('z')+1
+    color=['Violet','Indigo','Blue','Green','Yellow','Orange','Red']
+    while a<z:
+        i=1
+        while i<10:
+            ran_color=random.choice(color)
+            f.write('{}{},{}\n'.format(chr(a),i,ran_color))
+            i+=1
+        a+=1
+    f.close()
